c14555d7a6616dd52d8a2abfb1dc2b297326d19d
ShyShyTomato <88989002+ShyShyTomato@users.noreply.github.com>
2023-09-21 09:11:46 +1200

    Fixed run.py pep8

--- app/__init__.py
+++ app/__init__.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SamFeed.db'
 db = SQLAlchemy(app)
 
-#Grab all the things.
+# Grab all the things.
 from app import routes, models
 
-#Secret key for session management
+# Secret key for session management
 app.config['SECRET_KEY'] = 'lol'
-#app.app_context().push()
+# app.app_context().push()
