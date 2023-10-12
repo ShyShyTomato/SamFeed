bc8c4021f3bc2a8ba0915b823a9e00863083bbb7
ShyShyTomato <88989002+ShyShyTomato@users.noreply.github.com>
2023-10-13 09:11:57 +1300

    did a little cheeky security update because i felt like it, even though our project is done.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import secrets
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///SamFeed.db'
@@ -7,7 +8,9 @@ db = SQLAlchemy(app)
 
 # Grab all the things.
 from app import routes, models
+# Generate a secret key for session management
+secret_key = secrets.token_hex(16)
 
 # Secret key for session management
-app.config['SECRET_KEY'] = 'lol'
+app.config['SECRET_KEY'] = secret_key
 # app.app_context().push()
